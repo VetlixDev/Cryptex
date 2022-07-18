4c4440481cc5f6c3c244c8db56d7df8ea3279bbd
VetlixDev <80253872+VetlixDev@users.noreply.github.com>
2022-07-18 15:40:20 +0100

    Setup cogs for Cryptex

--- bot.py
+++ bot.py
@@ -7,5 +7,15 @@ bot = commands.Bot(command_prefix=os.getenv("PREFIX"), intents=nextcord.Intents.
 @bot.event
 async def on_ready():
 	print(f"{bot.user} is online!")
+	
+if __name__ == "__main__":
+	for filename in os.listdir("./cogs"):
+		if filename.endswith(".py"):
+			extension = filename[:-3]
+			try:
+				bot.load_extension(f"✅ | **cogs.{extension}** has been loaded!")
+			except:
+				exception = f"{type(e), __name__}: {e}"
+				print(f"❌ | {extension.capitalize()}!")
 
 bot.run(os.getenv("BOT_TOKEN"))
